b7e5cbe11303af4ca342052be18ea7c5d893d16b
kumaraayush67 <kumaraayush67@gmail.com>
2017-09-13 12:40:17 +0530

    Add files via upload

---
+++ Expo-myMod/main.js
@@ -0,0 +1,16 @@
+window.onload=function() {
+
+    $('.logo').hover(function(){
+        $(this).text("2017");
+       // $(this).style("padding","20px")
+    }, function() {
+        $(this).text("TECH EXPO");
+    });
+    $(".buttons").click(function(){
+ 
+
+    });
+
+
+
+};
